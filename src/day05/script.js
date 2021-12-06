78ca58b2ce7257f9e5b128e712ca052c8bb31d7c
ArthurTakase <arthur.decaen@epitech.eu>
2021-12-06 11:12:25 +0100

    day 5

--- src/day05/script.js
+++ src/day05/script.js
@@ -1,49 +1,5 @@
 const div = document.getElementById('result')
 const max = 989
-    // const max = 9
-
-// var input = [
-//     [
-//         [0, 9],
-//         [5, 9]
-//     ],
-//     [
-//         [8, 0],
-//         [0, 8]
-//     ],
-//     [
-//         [9, 4],
-//         [3, 4]
-//     ],
-//     [
-//         [2, 2],
-//         [2, 1]
-//     ],
-//     [
-//         [7, 0],
-//         [7, 4]
-//     ],
-//     [
-//         [6, 4],
-//         [2, 0]
-//     ],
-//     [
-//         [0, 9],
-//         [2, 9]
-//     ],
-//     [
-//         [3, 4],
-//         [1, 4]
-//     ],
-//     [
-//         [0, 0],
-//         [8, 8]
-//     ],
-//     [
-//         [5, 5],
-//         [8, 2]
-//     ]
-// ]
 
 function generateTab() {
     result = []
@@ -144,8 +100,6 @@ function day(input) {
     part01(input, generateTab())
     part02(input, generateTab())
 }
-
-// day(input)
 
 fetch("./input.json")
     .then(response => {
